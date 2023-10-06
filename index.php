3e5ce29f5f04d658963921b3d497b060487d11d8
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-06 15:25:55 -0300

    SafeZone

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+    include "teste.php"; // Chamando o código externo teste.php
+
+?>
+<p>Após o Include</p>
+
+<!-- Imprimindo conteúdo do código teste.php -->
+<p>O conteúdo do programa teste.php é <?php echo $c; ?></p>
---
+++ teste.php
@@ -0,0 +1,6 @@
+<?php
+$c = 5;
+
+?>
+
+<h1>Arquivo teste.php com a declaração da variável c</h1>
