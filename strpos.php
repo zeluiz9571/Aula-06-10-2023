167384e365ec2b1f4a1ba9f9ba8fadbbe35f0641
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-06 09:40:26 -0300

    Calunia

--- strpos.php
+++ strpos.php
@@ -2,8 +2,10 @@
 
 $text = "Numa toca no chão vivia um hobbit. Não uma toca desagradável, suja e úmida, cheia de restos de minhocas e com cheiro de lodo; tampouco uma toca seca, vazia e arenosa, sem nada em que sentar ou o que comer: era a toca de um hobbit, e isso quer dizer conforto.";
 
-$result = substr($text, 0, 27); //Posição inicial = 0, comprimento = 20
+$length = strpos($text, "com cheiro de lodo"); //Posição onde inicia a string "com cheiro de lodo" = 30
 
-echo $result
+$result = substr($text, 0, $length); //Posição inicial = 0, comprimento = 20
+
+echo $result;
 
 ?>
