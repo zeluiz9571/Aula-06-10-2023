165bbe4c853239402b9d2bbfd287ef952fd7deeb
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-06 15:52:28 -0300

    Professor

--- teste.php
+++ teste.php
@@ -4,3 +4,19 @@ $c = 5;
 ?>
 
 <h1>Arquivo teste.php com a declaração da variável c</h1>
+
+<div>
+    <h1>Encontre os melhores produtos:</h1>
+    <p>Lapiseira</p>
+    <p>Caneta BIC</p>
+    <p>Canetinha</p>
+    <p>Lapiseira</p>
+    <p>Caneta BIC</p>
+    <p>Canetinha</p>
+    <p>Lapiseira</p>
+    <p>Caneta BIC</p>
+    <p>Canetinha</p>
+    <p>Lapiseira</p>
+    <p>Caneta BIC</p>
+    <p>Canetinha</p>
+</div>
