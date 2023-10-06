30df4d08aa7cd9d8d0049810fd1bb795d3fca33d
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-06 09:31:48 -0300

    Create funcaosubstr.php

---
+++ funcaosubstr.php
@@ -0,0 +1,9 @@
+<?php
+
+$text = "Numa toca no chão vivia um hobbit. Não uma toca desagradável, suja e úmida, cheia de restos de minhocas e com cheiro de lodo; tampouco uma toca seca, vazia e arenosa, sem nada em que sentar ou o que comer: era a toca de um hobbit, e isso quer dizer conforto.";
+
+$result = substr($text, 0, 20); //Posição inicial = 0, comprimento = 20
+
+echo $result
+
+?>
